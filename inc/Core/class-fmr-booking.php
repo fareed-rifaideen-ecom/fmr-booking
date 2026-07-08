d79261e1e73c4e963f8d04657873391873e07ff8
Fareed M. Rifaideen <fareed.rifaideen@gmail.com>
2026-07-08 12:08:10 +0400

    Gem: Refactor FMR_Booking class structure
    Refactor FMR_Booking class to simplify initialization and registration of hooks.

--- inc/Core/class-fmr-booking.php
+++ inc/Core/class-fmr-booking.php
@@ -1,229 +1,87 @@
 <?php
-/**
- * The core plugin class.
- *
- * @link       https://fmr.com
- * @since      1.0.0
- * @package    FMR_Booking
- * @subpackage FMR_Booking/inc/Core
- */
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
 
-/**
- * The core plugin class.
- *
- * @package    FMR_Booking
- * @subpackage FMR_Booking/inc/Core
- * @author     FMR
- */
 class FMR_Booking {
 
-	/**
-	 * The loader that's responsible for maintaining and registering all hooks that power
-	 * the plugin.
-	 *
-	 * @access   protected
-	 * @var      FMR_Booking_Loader    $loader    Maintains and registers all hooks for the plugin.
-	 */
 	protected $loader;
-
-	/**
-	 * The unique identifier of this plugin.
-	 *
-	 * @access   protected
-	 * @var      string    $plugin_name    The string used to uniquely identify this plugin.
-	 */
 	protected $plugin_name;
-
-	/**
-	 * The current version of the plugin.
-	 *
-	 * @access   protected
-	 * @var      string    $version    The current version of the plugin.
-	 */
 	protected $version;
 
-	/**
-	 * Define the core functionality of the plugin.
-	 *
-	 * Set the plugin name and the plugin version that can be used throughout the plugin.
-	 * Load the dependencies, define the locale, and set the hooks for the admin area and
-	 * the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
 	public function __construct() {
-		if ( defined( 'FMR_BOOKING_VERSION' ) ) {
-			$this->version = FMR_BOOKING_VERSION;
-		} else {
-			$this->version = '1.0.0';
-		}
+		$this->version = defined( 'FMR_BOOKING_VERSION' ) ? FMR_BOOKING_VERSION : '1.0.0';
 		$this->plugin_name = 'fmr-booking';
-
-		$this->init_loader();
+		$this->loader = new FMR_Booking_Loader();
 	}
 
-	/**
-	 * Run the loader to execute all of the hooks with WordPress.
-	 *
-	 * @since    1.0.0
-	 */
 	public function run() {
 		$this->set_locale();
 		$this->define_admin_hooks();
 		$this->define_public_hooks();
 		$this->register_cron_hooks();
-
 		$this->loader->run();
 	}
 
-	/**
-	 * Initialize the loader.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
-	private function init_loader() {
-		$this->loader = new FMR_Booking_Loader();
-	}
-
-	/**
-	 * Define the locale for this plugin for internationalization.
-	 *
-	 * Uses the FMR_Booking_i18n class in order to set the domain and to register the hook
-	 * with WordPress.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
 	private function set_locale() {
 		$plugin_i18n = new FMR_Booking_i18n();
 		$this->loader->add_action( 'plugins_loaded', $plugin_i18n, 'load_plugin_textdomain' );
 	}
 
-	/**
-	 * Register all of the hooks related to the admin area functionality
-	 * of the plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
 	private function define_admin_hooks() {
 		$plugin_admin = new FMR_Booking_Admin( $this->get_plugin_name(), $this->get_version() );
-
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
 
-		// Client and Branding Admin Controller
-		$client_repo   = new FMR_Client_Repository();
-		$branding_repo = new FMR_Branding_Repository();
-		$admin_client  = new FMR_Admin_Client_Controller( $client_repo, $branding_repo );
-		$this->loader->add_action( 'admin_menu', $admin_client, 'register_menus' );
-
-		// Service and Resource Admin Controller
-		$service_repo  = new FMR_Service_Repository();
-		$resource_repo = new FMR_Resource_Repository();
-		$admin_service = new FMR_Admin_Service_Controller( $service_repo, $resource_repo );
-		$this->loader->add_action( 'admin_menu', $admin_service, 'register_menus' );
-
-		// Reminder Admin Controller
-		$notification_repo = new FMR_Notification_Repository();
-		$admin_reminder    = new FMR_Admin_Reminder_Controller( $notification_repo );
-		$this->loader->add_action( 'admin_menu', $admin_reminder, 'register_menus' );
-
-		// Approval Admin Controller
-		$approval_repo    = new FMR_Approval_Repository();
-		$log_repo         = new FMR_Log_Repository();
-		$availability_repo = new FMR_Availability_Repository();
-		$service_repo      = new FMR_Service_Repository();
-		$resource_repo     = new FMR_Resource_Repository();
-		$rule_repo         = new FMR_Rule_Repository();
+		// Controllers
+		$this->loader->add_action( 'admin_menu', new FMR_Admin_Client_Controller( new FMR_Client_Repository(), new FMR_Branding_Repository() ), 'register_menus' );
+		$this->loader->add_action( 'admin_menu', new FMR_Admin_Service_Controller( new FMR_Service_Repository(), new FMR_Resource_Repository() ), 'register_menus' );
+		$this->loader->add_action( 'admin_menu', new FMR_Admin_Reminder_Controller( new FMR_Notification_Repository() ), 'register_menus' );
 		
-		$availability_service = new FMR_Availability_Service( $availability_repo, $service_repo, $resource_repo, $rule_repo );
-		$booking_service      = new FMR_Booking_Service( $availability_service, $service_repo, $rule_repo );
-		$approval_service     = new FMR_Approval_Service( $approval_repo, $booking_service, $log_repo );
-		$admin_approval       = new FMR_Admin_Approval_Controller( $approval_repo, $approval_service );
+		$availability_service = new FMR_Availability_Service( new FMR_Availability_Repository(), new FMR_Service_Repository(), new FMR_Resource_Repository(), new FMR_Rule_Repository() );
+		$booking_service      = new FMR_Booking_Service( $availability_service, new FMR_Service_Repository(), new FMR_Rule_Repository() );
+		$approval_service     = new FMR_Approval_Service( new FMR_Approval_Repository(), $booking_service, new FMR_Log_Repository() );
 		
-		$this->loader->add_action( 'admin_menu', $admin_approval, 'register_menus' );
+		$this->loader->add_action( 'admin_menu', new FMR_Admin_Approval_Controller( new FMR_Approval_Repository(), $approval_service ), 'register_menus' );
 	}
 
-	/**
-	 * Register all of the hooks related to the public-facing functionality
-	 * of the plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
 	private function define_public_hooks() {
 		$plugin_public = new FMR_Booking_Public( $this->get_plugin_name(), $this->get_version() );
-
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
+		$this->loader->add_action( 'init', new FMR_Booking_Shortcode( new FMR_Service_Repository(), new FMR_Client_Repository() ), 'register' );
 
-		// Shortcode
-		$service_repo = new FMR_Service_Repository();
-		$client_repo  = new FMR_Client_Repository();
-		$shortcode    = new FMR_Booking_Shortcode( $service_repo, $client_repo );
-		$this->loader->add_action( 'init', $shortcode, 'register' );
-
-		// REST API Integration
-		$availability_repo = new FMR_Availability_Repository();
-		$service_repo      = new FMR_Service_Repository();
-		$resource_repo     = new FMR_Resource_Repository();
-		$rule_repo         = new FMR_Rule_Repository();
-		
-		$availability_service = new FMR_Availability_Service( $availability_repo, $service_repo, $resource_repo, $rule_repo );
-		$booking_service      = new FMR_Booking_Service( $availability_service, $service_repo, $rule_repo );
-		$rest_controller      = new FMR_Booking_REST_Controller( $availability_service, $booking_service );
-		$this->loader->add_action( 'rest_api_init', $rest_controller, 'register_routes' );
-
-		// WooCommerce Integration
-		$wc_adapter = new FMR_WooCommerce_Adapter( $booking_service );
-		$wc_adapter->init();
+		// 🚨 FIX: Defer REST and WooCommerce instantiation
+		$this->loader->add_action( 'rest_api_init', $this, 'register_rest_controllers' );
+		$this->loader->add_action( 'plugins_loaded', $this, 'register_woocommerce_integration' );
 	}
 
-	/**
-	 * Register cron hooks (execution only, not scheduling).
-	 */
+	public function register_rest_controllers() {
+		$availability_service = new FMR_Availability_Service( new FMR_Availability_Repository(), new FMR_Service_Repository(), new FMR_Resource_Repository(), new FMR_Rule_Repository() );
+		$rest_controller = new FMR_Booking_REST_Controller( 
+			$availability_service, 
+			new FMR_Booking_Service( $availability_service, new FMR_Service_Repository(), new FMR_Rule_Repository() ) 
+		);
+		$rest_controller->register_routes();
+	}
+
+	public function register_woocommerce_integration() {
+		if ( class_exists( 'WooCommerce' ) ) {
+			$availability_service = new FMR_Availability_Service( new FMR_Availability_Repository(), new FMR_Service_Repository(), new FMR_Resource_Repository(), new FMR_Rule_Repository() );
+			$wc_adapter = new FMR_WooCommerce_Adapter( new FMR_Booking_Service( $availability_service, new FMR_Service_Repository(), new FMR_Rule_Repository() ) );
+			$wc_adapter->init();
+		}
+	}
+
 	private function register_cron_hooks() {
 		add_action( 'fmr_process_reminders', function() {
-			$notification_repo    = new FMR_Notification_Repository();
-			$branding_repo        = new FMR_Branding_Repository();
-			$branding_service     = new FMR_Branding_Service( $branding_repo );
-			$notification_service = new FMR_Notification_Service( $notification_repo, $branding_service );
-			$cron_service         = new FMR_Cron_Service( $notification_repo, $notification_service );
+			$notification_repo = new FMR_Notification_Repository();
+			$cron_service = new FMR_Cron_Service( $notification_repo, new FMR_Notification_Service( $notification_repo, new FMR_Branding_Service( new FMR_Branding_Repository() ) ) );
 			$cron_service->process_reminders();
 		} );
 	}
 
-	/**
-	 * The name of the plugin used to uniquely identify it within the context of
-	 * WordPress and to define internationalization functionality.
-	 *
-	 * @since     1.0.0
-	 * @return    string    The name of the plugin.
-	 */
-	public function get_plugin_name() {
-		return $this->plugin_name;
-	}
-
-	/**
-	 * The reference to the class that orchestrates the hooks with the core plugin.
-	 *
-	 * @since     1.0.0
-	 * @return    FMR_Booking_Loader    Orchestrates the hooks of the plugin.
-	 */
-	public function get_loader() {
-		return $this->loader;
-	}
-
-	/**
-	 * Retrieve the version number of the plugin.
-	 *
-	 * @since     1.0.0
-	 * @return    string    The version number of the plugin.
-	 */
-	public function get_version() {
-		return $this->version;
-	}
-
+	public function get_plugin_name() { return $this->plugin_name; }
+	public function get_loader() { return $this->loader; }
+	public function get_version() { return $this->version; }
 }
